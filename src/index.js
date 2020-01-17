a953fcae1edbc7007ed7549d85ecbc7ad355893e
Patrick Thomaz <patrick_thomaz@MacBook-Air-de-Patrick.local>
2020-01-17 16:37:20 -0300

    Add Cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-yhxaf.mongodb.net/o
   useCreateIndex: true,
 });
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
